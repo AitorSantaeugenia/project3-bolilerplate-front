0027baf80cd756c6e933d81416d25e57bc0eb237
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-05-26 19:22:30 +0200

    added title and description

--- src/pages/ProjectDetailsPage.js
+++ src/pages/ProjectDetailsPage.js
@@ -1,87 +1,76 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 // import axios from "axios";
-import {getProjectDetailsService} from "../services/project.services";
+import { getProjectDetailsService } from '../services/project.services';
 
-import { Link, useParams } from "react-router-dom";
-import AddTask from "../components/AddTask";
+import { Link, useParams } from 'react-router-dom';
+import AddTask from '../components/AddTask';
 
-import TaskCard from "../components/TaskCard";
+import TaskCard from '../components/TaskCard';
 
 const API_URL = process.env.REACT_APP_API_URL;
 
+function ProjectDetailsPage(props) {
+	const [ project, setProject ] = useState(null);
+	const { id } = useParams();
+	//const projectId = props.match.params.id;
+	const projectId = id;
 
-function ProjectDetailsPage (props) {
-  const [project, setProject] = useState(null);
-  const { id } = useParams();
-  //const projectId = props.match.params.id;
- const projectId = id;
-  
-  
-  const getProject = async () => {
-    //console.log(id)
-    // Get the token from the localStorage
-    //const storedToken = localStorage.getItem('authToken');
-    const storedToken = localStorage.getItem('authToken');
-    try{
-      const response = await getProjectDetailsService(id)
-      setProject(response.data)
-    }catch(err){
-      console.log(err)
-    }
+	const getProject = async () => {
+		//console.log(id)
+		// Get the token from the localStorage
+		//const storedToken = localStorage.getItem('authToken');
+		const storedToken = localStorage.getItem('authToken');
+		try {
+			const response = await getProjectDetailsService(id);
+			setProject(response.data);
+		} catch (err) {
+			console.log(err);
+		}
 
-    // Send the token through the request "Authorization" Headers
-    
-    // axios
-    //   .get(
-    //     `${API_URL}/projects/${projectId}`,
-    //     { headers: { Authorization: `Bearer ${storedToken}` } }
-    //   )
-    //   .then((response) => {
-    //     const oneProject = response.data;
-    //     console.log(oneProject)
-    //     setProject(oneProject);
-    //   })
-    //   .catch((error) => console.log(error));
-  };
-  
-  
-  useEffect(()=> {
-    getProject();
-  }, [] );
+		// Send the token through the request "Authorization" Headers
 
-  
-  return (
-    
-    <div className="ProjectDetails">
-      
-      {project && (
-        <>
-          <h1>{project.title}</h1>
-          <p>{project.description}</p>
-        </>
-      )}
+		// axios
+		//   .get(
+		//     `${API_URL}/projects/${projectId}`,
+		//     { headers: { Authorization: `Bearer ${storedToken}` } }
+		//   )
+		//   .then((response) => {
+		//     const oneProject = response.data;
+		//     console.log(oneProject)
+		//     setProject(oneProject);
+		//   })
+		//   .catch((error) => console.log(error));
+	};
 
-      
-      <AddTask refreshProject={getProject} projectId={projectId} />          
-        
-      { project && project.tasks.map((task) => {
-        return(
+	useEffect(() => {
+		getProject();
+	}, []);
 
-          <TaskCard  key={task._id} {...task} />  
-        )
-    
-      })}
+	return (
+		<div className="ProjectDetails">
+			{project && (
+				<div>
+					<h1>Project: {project.title}</h1>
+					<p>Description: {project.description}</p>
+				</div>
+			)}
 
-      <Link to="/projects">
-        <button>Back to projects</button>
-      </Link>
-          
-      <Link to={`/projects/edit/${projectId}`}>
-        <button>Edit Project</button>
-      </Link>
-      
-    </div>
-  );
+			<AddTask refreshProject={getProject} projectId={projectId} />
+
+			{project &&
+				project.tasks.map((task) => {
+					return <TaskCard key={task._id} {...task} />;
+				})}
+
+			<Link to="/projects">
+				<button>Back to projects</button>
+			</Link>
+
+			<Link to={`/projects/edit/${projectId}`}>
+				<button>Edit Project</button>
+			</Link>
+		</div>
+	);
 }
 
 export default ProjectDetailsPage;
