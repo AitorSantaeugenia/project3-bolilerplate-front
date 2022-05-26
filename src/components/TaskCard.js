c99a5090db4d077020a2e00d96bbcc6c4a08b11d
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-05-26 19:22:02 +0200

    added task services

--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -1,51 +1,79 @@
 // We are deconstructing the props object directly in the parentheses of the function
-import axios from 'axios'
-import React, {useState } from 'react'
+import axios from 'axios';
+import React, { useState } from 'react';
 
+import { updateTaskService, deleteTaskService } from '../services/tasks.services';
 
 function TaskCard({ title, description, _id }) {
-  const [inputTitle, setInputTitle] = useState(title)
-  const [inputDescription, setInputDescription] = useState(description)
-  const [isDeleted, setIsDeleted] = useState(false)
-  const API_URL = process.env.REACT_APP_API_URL;
-  const storedToken = localStorage.getItem('authToken');
+	const [ inputTitle, setInputTitle ] = useState(title);
+	const [ inputDescription, setInputDescription ] = useState(description);
+	const [ isDeleted, setIsDeleted ] = useState(false);
+	const API_URL = process.env.REACT_APP_API_URL;
+	const storedToken = localStorage.getItem('authToken');
+	const idTask = _id;
 
+	const handleSubmitTask = async (e) => {
+		try {
+			const requestBody = { inputTitle, inputDescription };
 
-  const handleSubmitTask =(e)=>{
-    axios.put(`${API_URL}/tasks/${_id}`, 
-      {
-        title: inputTitle,
-        description: inputDescription
-      },
-      { headers: { Authorization: `Bearer ${storedToken}` } }
-    )
-    .then((response)=>{console.log(response.data)})
-  }
+			const response = await updateTaskService(idTask, requestBody);
+			// Reset the state to clear the inputs
+			// setProject(response.data);
+			console.log('Updated task', requestBody);
+		} catch (err) {
+			console.log(err);
+		}
+		// axios
+		// 	.put(
+		// 		`${API_URL}/tasks/${_id}`,
+		// 		{
+		// 			title: inputTitle,
+		// 			description: inputDescription
+		// 		},
+		// 		{ headers: { Authorization: `Bearer ${storedToken}` } }
+		// 	)
+		// 	.then((response) => {
+		// 		console.log(response.data);
+		// 	});
+	};
 
-  const handleDeleteTask = (e) =>{
-    axios.delete(`${API_URL}/tasks/${_id}`, 
-      { headers: { Authorization: `Bearer ${storedToken}` } }
-    )
-    .then((response)=>{
-      console.log(response.data)
-      setIsDeleted(true)
-    })
+	const handleDeleteTask = async (e) => {
+		try {
+			const response = await deleteTaskService(idTask);
+			setIsDeleted(true);
+		} catch (err) {
+			console.log(err);
+		}
+		// axios
+		// 	.delete(`${API_URL}/tasks/${_id}`, { headers: { Authorization: `Bearer ${storedToken}` } })
+		// 	.then((response) => {
+		// 		setIsDeleted(true);
+		// 	});
+	};
 
-  }
-
-  return (
-    <>
-    {!isDeleted &&
-    <div className="TaskCard card">
-      <input value ={inputTitle} onChange={(e)=>{setInputTitle(e.target.value)}}/>
-      <input value ={inputDescription} onChange={(e)=>{setInputDescription(e.target.value)}}/>
-      <button onClick={handleSubmitTask}>Modificar</button>
-      <button onClick={handleDeleteTask}>Eliminar</button>
-    </div>
-  }
-  </>
-  );
+	return (
+		<div>
+			{!isDeleted && (
+				<div className="TaskCard card">
+					<p>Task</p>
+					<input
+						value={inputTitle}
+						onChange={(e) => {
+							setInputTitle(e.target.value);
+						}}
+					/>
+					<input
+						value={inputDescription}
+						onChange={(e) => {
+							setInputDescription(e.target.value);
+						}}
+					/>
+					<button onClick={handleSubmitTask}>Modificar</button>
+					<button onClick={handleDeleteTask}>Eliminar</button>
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default TaskCard;
-
